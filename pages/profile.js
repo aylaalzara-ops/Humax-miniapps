9e0fd7ff51c49a1632caef43058da156aed9f647
aylaalzara-ops <aylaalzara@gmail.com>
2026-05-06 22:38:20 +0800

    Create profile.js

---
+++ pages/profile.js
@@ -0,0 +1,8 @@
+export default function Profile() {
+  return (
+    <div style={{ textAlign: "center", paddingTop: 100 }}>
+      <h2>Profile</h2>
+      <p>Wallet connected user</p>
+    </div>
+  );
+}
