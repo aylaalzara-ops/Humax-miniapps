93266b56b73374695a7211f66e8dd71a114ce41e
aylaalzara-ops <aylaalzara@gmail.com>
2026-05-19 10:57:55 +0800

    Humax miniapp

--- pages/profile.js
+++
@@ -1,8 +0,0 @@
-export default function Profile() {
-  return (
-    <div style={{ textAlign: "center", paddingTop: 100 }}>
-      <h2>Profile</h2>
-      <p>Wallet connected user</p>
-    </div>
-  );
-}
